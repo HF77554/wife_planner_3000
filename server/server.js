48544eb2f4d010dd975201503232839b45507d59
Hugo <HF77554@gmail.com>
2021-10-13 17:31:20 -0400

    server and database original set up, no routes set up yet

--- server/server.js
+++ server/server.js
@@ -1,6 +1,15 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const port = 3000
+
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true  })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
