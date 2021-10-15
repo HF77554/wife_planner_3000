f114fb1a739cd39b491981239706248701bf8ab4
Hugo <HF77554@gmail.com>
2021-10-15 16:58:52 -0400

    steps taken to set up and start organizing front end (Planning)

---
+++ client/src/components/AuthenticationPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UserRelationshipRole from './UserRelationshipRole'
+
+const AuthenticationPage = () => {
+    return (
+        <div>
+            <h2>Authentication</h2>
+            <UserRelationshipRole />
+        </div>
+    )
+}
+
+export default AuthenticationPage
