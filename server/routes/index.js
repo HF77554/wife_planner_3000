87f9546d4921132d995d140c95c5edb51f40a3d1
Ricardo Spear <spear.ricardo@gmail.com>
2021-10-19 14:18:54 -0500

    implement bootstraping functionality, dynamic route registration and security for express

---
+++ server/routes/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+module.exports = async (app) => {
+    // Get list of routers
+    const files = await fs.readDir(__dirname);
+
+    //iterate through routers
+    files.forEach(file => {
+
+        // make sure the file is JS and not this file
+        if (!file == 'index.js' && file.endsWith('.js')) {
+
+            // get route subscriber 
+            routerSubscriber = require(path.join(__dirname, file));
+
+            // check if route subscriber exists (UserTasks would not exist right now)
+            if (routerSubscriber && typeof routerSubscriber == 'function') {
+                routerSubscriber(app);
+                console.log(`Router ${file} was subscribed`)
+            } else {
+                console.error(`Router ${file} does not export a subscriber`);
+            }
+        }
+    })
+};
+  
