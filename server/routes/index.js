d3c01bc9141a37f33060e85bbebb8d7e51ccc92c
Hugo <HF77554@gmail.com>
2021-10-21 14:57:50 -0400

    database encryption set up, user can now log in thought POST

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,10 +9,10 @@ module.exports = async (app) => {
     files.forEach(file => {
         // make sure the file is JS and not this file
         if (file != 'index.js' && file.endsWith('.js')) {
-
+            
             // get route subscriber 
             routerSubscriber = require(path.join(__dirname, file));
-
+            
             // check if route subscriber exists (UserTasks would not exist right now)
             if (routerSubscriber && typeof routerSubscriber == 'function') {
                 routerSubscriber(app);
